72bbf022802308c4b65da90babd7a8a32dcb3d3d
Alexandra_B <163668447+littlestudent777@users.noreply.github.com>
2024-05-04 21:25:26 +0300

    Added files for lab2 and README.md

---
+++ lab2/main.c
@@ -0,0 +1,24 @@
+﻿#include "treap.h"
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    // Make an example of a treap
+    Node* root = NULL;
+    int i;
+    for (i = 1; i <= 10; i++) {
+        root = insertNode(root, i);
+    }
+    printBrackStr(root);
+    printf("\n");
+    //    printTree(root);
+    //    printf("\n");
+        // Erase an element and print the result
+    root = deleteNode(root, 6);
+    printf("Result after deleting key == 6:\n");
+    printBrackStr(root);
+
+    freeTree(root);
+    return 0;
+}
